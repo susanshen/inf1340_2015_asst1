71cef07dc9c6529602e2db0cc52aeb24a946a682
susanshen <shuyun.shen@mail.utoronto.ca>
2015-10-08 11:13:27 -0400

    added test cases

--- exercise2.py
+++ exercise2.py
@@ -30,44 +30,57 @@ def name_that_shape(user_input):
 #Input: A phrase to request user's input and a text box for users to input a number
 #Error: Input limited to numbers between 3-10, any other numbers will result in an error.
 
-user_input = int(raw_input('Enter the number of sides of a regular polygon and we will name that shape:'))
-
+#Test Cases:
 #Input: 3
 #Expected Output: Triangle
-if user_input == 3:
-    print('Triangle')
+
 #Input: 4
 #Expected Output: Square
-elif user_input == 4:
-    print('Square')
+
 #Input: 5
 #Expected Output: Pentagon
-elif user_input == 5:
-    print('Pentagon')
+
 #Input: 6
 #Expected Output: Hexagon
-elif user_input == 6:
-    print('Hexagon')
+
 #Input: 7
 #Expected Output: Heptagon
-elif user_input == 7:
-    print('Heptagon')
+
 #Input: 8
 #Expected Output: Octagon
-elif user_input == 8:
-    print('Octagon')
+
 #Input: 9
 #Expected Output: Nonagon
-elif user_input == 9:
-    print('Nonagon')
+
 #Input: 10
 #Expected Output: Decagon
+
+#Input: <3
+#Expected Output: Error: This is not a regular polygon
+
+#Input:>10
+#Expected Output: Error: This is not a regular polygon
+
+user_input = int(raw_input('Enter the number of sides of a regular polygon and we will name that shape:'))
+
+if user_input == 3:
+    print('Triangle')
+elif user_input == 4:
+    print('Square')
+elif user_input == 5:
+    print('Pentagon')
+elif user_input == 6:
+    print('Hexagon')
+elif user_input == 7:
+    print('Heptagon')
+elif user_input == 8:
+    print('Octagon')
+elif user_input == 9:
+    print('Nonagon')
 elif user_input == 10:
     print('Decagon')
-
 elif user_input > 10:
     print('Error: This program can only name shapes that have up to 10 sides')
-
 elif user_input < 3:
     print('Error: This is not a regular polygon')
 
